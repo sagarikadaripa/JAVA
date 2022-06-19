8b22dd0438a0405b66fb3e2a00c65ef07dac2e0f
sagarikadaripa <sagarikadaripa@gmail.com>
2022-06-19 23:08:49 +0530

    diamond pattern updated

--- src/JavaPatterns/diamond.java
+++ src/JavaPatterns/diamond.java
@@ -13,5 +13,26 @@ package JavaPatterns;
 public class diamond {
     public static void main(String[] args){
 
+        int n = 4;
+        //1st half
+        for(int i = 1;i <= n; i++){
+            for(int  j = 1;j <= n - i ;j++){
+                System.out.print(" "); //spaces before stars
+            }
+            for(int j = 1;j <= 2*i - 1;j++){
+                System.out.print("*"); //stars
+            }
+            System.out.println();
+        }
+        //2nd half
+        for(int i = n;i >= 1; i--){
+            for(int  j = 1;j <= n - i ;j++){
+                System.out.print(" "); //spaces before stars
+            }
+            for(int j = 1;j <= 2*i - 1;j++){
+                System.out.print("*"); //stars
+            }
+            System.out.println();
+        }
     }
 }
